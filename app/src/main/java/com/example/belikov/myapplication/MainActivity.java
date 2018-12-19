a19089d71200def5feb70b47fff5c69772dffce0
Belikov <Sergeybelik86@gmail.com>
2018-12-19 15:30:38 +0300

    ДЗ №1 немного стилей

--- app/src/main/java/com/example/belikov/myapplication/MainActivity.java
+++ app/src/main/java/com/example/belikov/myapplication/MainActivity.java
@@ -107,4 +107,9 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void onAvatarClick(View view){
+        Toast.makeText(this, "avatar", Toast.LENGTH_SHORT).show();
+        //FIXME
+    }
 }
